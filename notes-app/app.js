144a1ae189e7254772674989e06ad070b6ed74e7
kbventures <kmb_x@hotmail.com>
2021-07-29 16:38:06 +0200

    Adding a note functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
-
+// const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 yargs.version('1.1.0');
 
 // Create add command
@@ -22,7 +22,8 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title: ' + argv.title + 'Body: ' + argv.body);
+    // console.log('Title: ' + argv.title + 'Body: ' + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
