ec70b48ffe96cf683c6501614f35adf34a7bd74e
kbventures <kmb_x@hotmail.com>
2021-09-16 15:44:42 +0200

    Customizing HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,21 @@
 const request = require('request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=49088faa4578fd65877d97fbf6281e52&query=37.8267,-122.4233';
+  'http://api.weatherstack.com/current?access_key=49088faa4578fd65877d97fbf6281e52&query=37.8267,-122.4233&units=f';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.current;
+  console.log(data);
+});
+
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.current;
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ' It is currently ' +
+      data.temperature +
+      ' degrees out. There is ' +
+      data.precip +
+      ' % chance of rain.'
+  );
 });
