5f2582a0548ea2027a74e89ccac76e1615bc12a4
kbventures <kmb_x@hotmail.com>
2021-09-19 16:34:38 +0200

    es6 Object Property Short Hand and Destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,48 @@
+// Object property shorthand
+
+const name = 'Andrew';
+const userAge = 27;
+
+const user = {
+  name,
+  userAge,
+  location: 'Philadelphia',
+};
+
+console.log(user);
+
+// Ojbect destructing
+
+// let { name, userAge, location } = user;
+
+// console.log(name, userAge, location);
+
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined,
+  rating: 6,
+};
+
+// let { label, price, stock, salePrice } = product;
+
+// let {
+//   label: aLabel,
+//   price: aPrice,
+//   stock: aStock,
+//   salePrice: AsalePrice,
+//   rating = 5,
+// } = product;
+
+// console.log(product);
+
+// console.log(label, price, stock, salePrice);
+
+// console.log(aLabel, aPrice, aStock, AsalePrice, rating);
+
+const transaction = (type, { label, stock }) => {
+  console.log(label, stock);
+};
+
+transaction('order', product);
