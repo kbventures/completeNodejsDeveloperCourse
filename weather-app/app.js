724ebe7050049093bd4bcf47fc42c918de325f58
kbventures <kmb_x@hotmail.com>
2021-09-19 15:50:19 +0200

    Callback chaining challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,23 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-// forecast(-75.7088, 44.1545, (error, data) => {
-//   console.log('Error', error);
-//   console.log('Data', data);
-// });
+const address = process.argv[2];
 
-geocode('Boston', (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+  console.log('Please provice an address');
+} else {
+  geocode(address, (error, data) => {
     if (error) {
       return console.log(error);
     }
 
-    console.log(data.location);
-    console.log(forecastData);
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
   });
-});
+}
