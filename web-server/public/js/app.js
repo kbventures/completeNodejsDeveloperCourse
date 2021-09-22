4f644470131cbf3d7ace40276bf9dccd54cc2954
kbventures <kmb_x@hotmail.com>
2021-09-22 21:02:59 +0200

    Browser HTTP Request using Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client Side Javascript file is Loaded');
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location, data.forecast);
+    }
+  });
+});
