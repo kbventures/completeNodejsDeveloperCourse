31e6dc8a8fa3eb0749a28100b4f201347d05ba87
kbventures <kmb_x@hotmail.com>
2021-09-22 16:37:14 +0200

    The Query String

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -44,16 +44,29 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  res.send([
-    {
-      location: 'Boston',
-      forecast: 'Cloudy',
-    },
-    {
-      location: 'New York',
-      forecast: 'Sunny',
-    },
-  ]);
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  }
+  res.send({
+    location: req.query.address,
+  });
+});
+
+app.get('/products', (req, res) => {
+  //localhost:3000/products?search=games&rating=5
+  console.log(req.query);
+
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
 });
 
 app.get('/help/*', (req, res) => {
