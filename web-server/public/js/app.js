22aeeec239c970c2019588fe36b450f7b4489006
kbventures <kmb_x@hotmail.com>
2021-09-26 01:16:38 +0200

    wiringUpUserInterface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,31 +1,29 @@
 console.log('Client Side Javascript file is Loaded');
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location, data.forecast);
-    }
-  });
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+// messageOne.textContent = 'From Javascript';
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const location = search.value;
-  console.log(location);
+
+  messageOne.textContent = 'Loading Weather information';
+  messageTwo.textContent = '';
 
   fetch('http://localhost:3000/weather?address=' + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          messageOne.textContent = '';
+          messageTwo.textContent = data.error;
         } else {
-          console.log(data.location, data.forecast);
+          messageTwo.textContent = '';
+          messageOne.textContent = data.location + ' ' + data.forecast;
         }
       });
     }
