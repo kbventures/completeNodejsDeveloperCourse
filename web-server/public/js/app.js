219c3ea44b6527954fa3ea28581081db24e709bb
kbventures <kmb_x@hotmail.com>
2021-09-26 00:36:38 +0200

    Created A search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -9,3 +9,25 @@ fetch('http://localhost:3000/weather?address=!').then((response) => {
     }
   });
 });
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+  console.log(location);
+
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log(data.location, data.forecast);
+        }
+      });
+    }
+  );
+});
